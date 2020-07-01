b6b44fa1788b1ca0b94cce5485fe4b64fc98b711
Hirumago <kevin.guillier.pro@gmail.com>
2020-07-01 16:30:59 +0200

    pokemon added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import {PokemonsModule} from "./pokemons/pokemons.module";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), PokemonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
